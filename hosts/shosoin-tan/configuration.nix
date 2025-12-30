65f065e46abdb51a7ac5c574e4d8abe77dc4e2b7
t3u <t3u@t3u.uk>
2025-12-31 01:37:25 +0900

    feat(shosoin-tan): enable sops-nix and align configuration with kagutsuchi-sama

--- hosts/shosoin-tan/configuration.nix
+++ hosts/shosoin-tan/configuration.nix
@@ -1,5 +1,8 @@
 { config, pkgs, inputs, ... }:
 
+let
+  username = "t3u";
+in
 {
   imports = [
     ./disko-config.nix
@@ -9,6 +12,24 @@
   boot.kernelPackages = pkgs.linuxPackages;
 
   nixpkgs.config.allowUnfree = true;
+
+  # SOPS configuration
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  sops.age.sshKeyPaths = [ ];
+  sops.age.generateKey = false;
+
+  environment.variables = {
+    SOPS_AGE_KEY_FILE = "/var/lib/sops-nix/key.txt";
+  };
+
+  sops.secrets.shosoin_tan_t3u_password_hash = {
+    neededForUsers = true;
+  };
+  sops.secrets.shosoin_tan_root_password_hash = {
+    neededForUsers = true;
+  };
 
   # Bootloader configuration
   boot.loader.grub = {
@@ -47,13 +68,25 @@
     };
   };
 
-  users.users.t3u = {
+  users.mutableUsers = false;
+
+  users.users.${username} = {
     isNormalUser = true;
     extraGroups = [ "wheel" "video" "render" ];
+    hashedPasswordFile = config.sops.secrets.shosoin_tan_t3u_password_hash.path;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3QNRSxPauISsWs7nob0tXfxjTsMpBEIYIjasRD9bpT t3u@BrokenPC"
     ];
   };
+
+  users.users.root = {
+    hashedPasswordFile = config.sops.secrets.shosoin_tan_root_password_hash.path;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3QNRSxPauISsWs7nob0tXfxjTsMpBEIYIjasRD9bpT t3u@BrokenPC"
+    ];
+  };
+
+  nix.settings.trusted-users = [ "root" "t3u" ];
 
   system.stateVersion = "25.05";
 }
