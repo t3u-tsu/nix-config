8ba4abee0facffded02fa5035bb7c2afd873f648
t3u <t3u@t3u.uk>
2026-01-04 14:46:43 +0900

    fix: correct syntax error in discord-bridge.nix

--- hosts/shosoin-tan/services/discord-bridge.nix
+++ hosts/shosoin-tan/services/discord-bridge.nix
@@ -7,7 +7,15 @@
       # ID is overridden by DISCORD_ADMIN_GUILD_ID env var
       discord.admin_guild_id = "@ADMIN_GUILD_ID@";
       database.path = "/var/lib/minecraft-discord-bridge/bridge.db";
-...
+      bridge.socket_path = "/run/minecraft-discord-bridge/bridge.sock";
+      servers.nitac23s = {
+        network = "tcp";
+        address = "127.0.0.1:25575";
+      };
+    };
+    environmentFile = config.sops.secrets.discord_bridge_env.path;
+  };
+
   sops.secrets.discord_bridge_env = {
     owner = "minecraft";
   };
